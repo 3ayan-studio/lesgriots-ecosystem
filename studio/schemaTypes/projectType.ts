c12d932858080c8066a0e43d6748842cd98f0966
Habib Hadjar <habibhadjar@MacBook-Pro-de-Habib.local>
2026-02-06 14:25:25 +0100

    Setup deployment pipeline, agency only for now

---
+++ studio/schemaTypes/projectType.ts
@@ -0,0 +1,26 @@
+import { defineField, defineType } from 'sanity'
+
+export const projectType = defineType({
+    name: 'project',
+    title: 'Project',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'title',
+            type: 'string',
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            name: 'slug',
+            type: 'slug',
+            options: { source: 'title' },
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            name: 'publishedAt',
+            type: 'datetime',
+            initialValue: () => new Date().toISOString(),
+            validation: (rule) => rule.required(),
+        })
+    ]
+})
