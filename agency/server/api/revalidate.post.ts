c80ece1d0ffabe134e525931bcc147024a5916b4
Habib Hadjar <habibhadjar@MacBook-Pro-de-Habib.local>
2026-03-26 19:36:45 +0100

    further testing

--- agency/server/api/revalidate.post.ts
+++ agency/server/api/revalidate.post.ts
@@ -2,6 +2,7 @@ export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig(event);
 
     const secret = config.deploySecret;
+    console.log("secret", secret);
     const authHeader = getHeader(event, "authorization");
     if (!secret) {
         console.error("🚨 DEPLOY_SECRET is not set in environment variables!");
